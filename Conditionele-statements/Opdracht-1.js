39be3f4a25eda423ad040af3b4839335c7d05f80
Ninjasp3ctr3 <141743215+Ninjasp3ctr3@users.noreply.github.com>
2024-10-24 18:59:16 +0200

    Mijn oplossing voor opdracht 1 van Conditionele statements

---
+++ Conditionele-statements/Opdracht-1.js
@@ -0,0 +1,16 @@
+const docent = "Jordy";
+
+switch (docent) {
+  case "Mark":
+    console.log(`Hallo Mark!`);
+    break;
+  case "John":
+    console.log(`Hallo John!`);
+    break;
+  case "Jordy":
+    console.log(`Hallo Jordy`);
+    break;
+  default:
+    console.log(`Hallo vreemdeling!`);
+    break;
+}
