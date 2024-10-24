4dbdf45335860171de46028eccf92074016f0dec
Ninjasp3ctr3 <141743215+Ninjasp3ctr3@users.noreply.github.com>
2024-10-24 19:03:07 +0200

    Mijn oplossing voor opdracht 1 van iteratieve statements

---
+++ Iteratieve-Statements/Opdracht-1.js
@@ -0,0 +1,21 @@
+const cursisten = [
+  "Angelique",
+  "Arne",
+  "Domenico",
+  "Ian",
+  "Jochen",
+  "Jorg",
+  "Marco",
+  "Marvi",
+  "Michelle",
+  "Romy",
+  "Simone",
+  "Tony",
+  "Yoeri",
+];
+let i = 0;
+
+while (i < cursisten.length) {
+  console.log(cursisten[i]);
+  i++;
+}
